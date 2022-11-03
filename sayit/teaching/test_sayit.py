f22695fd25e7b8cbaafad4555c4a14839a0a643d
Brazz <42916786+acbrazzl@users.noreply.github.com>
2022-11-03 07:24:16 -0400

    Update test_sayit.py
    added unittest of app-level

--- sayit/teaching/test_sayit.py
+++ sayit/teaching/test_sayit.py
@@ -3,11 +3,18 @@ import unittest
 import argparse
 import sayit
 
+args = sayit.get_parser().parse_args()
+
 class TestSayit(unittest.TestCase):
 
     def test_say(self):
         sayit.say('test_success')
 
-    #TODO: implement failure test of say
+    #TODO: (2) implement failure test of say
 
-    #TODO: implement argparse wrap of sayit.py
+    def test_sayit_empty_args(self):
+        with self.assertRaises(SystemExit):
+            sayit.main(args)
+
+if __name__ == '__main__':
+    unittest.main()
